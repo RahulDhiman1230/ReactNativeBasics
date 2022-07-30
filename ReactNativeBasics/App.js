cb05c8a1fee093ab14b1e8aaf248e6adec378611
Rahul Dhiman <rahul.dhiman@appinventiv.com>
2022-07-30 17:54:26 +0530

    wip

--- ReactNativeBasics/App.js
+++ ReactNativeBasics/App.js
@@ -8,6 +8,7 @@
 
 import React, { useState } from 'react';
 import {
+  Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -23,6 +24,22 @@ import {
 
 export const App = () => {
  const [goalInput, setGoalInput] = useState('');
+ const [goalsArray, setGoalsArray] = useState([]);
+
+/**
+ * when the user will click on add
+ * The current text will be added to the goals array with a _id
+ */
+ const onAdd = () => {
+    const addObj = {
+      _id: Math.random().toString(),
+      title: goalInput  
+    };
+
+    setGoalsArray( goalsArray => [...goalsArray,addObj] );
+    setGoalInput('');
+ };
+
   return(
     <SafeAreaView style={styles.mainContainer} >
         <Text style={styles.headerText} >{"Add You Daily Goal's"}</Text>
@@ -38,9 +55,18 @@ export const App = () => {
             autoCorrect={false}
           />
 
-          <TouchableOpacity style={styles.button} >
+          <TouchableOpacity onPress={onAdd} disabled={ goalInput?.length === 0 } style={styles.button} >
             <Text style={styles.btnText} >{'ADD'}</Text>
           </TouchableOpacity>
+        </View>
+
+        <View style={styles.listView} >
+          {
+            goalsArray?.map((item) => <Pressable style={({pressed}) => {
+              pressed && styles.pressedStyle
+            }} >
+              <Text style={styles.listText} key={ item?._id } >{item?.title}</Text></Pressable> )
+          }
         </View>
     </SafeAreaView>
   );
@@ -83,6 +109,20 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     color: 'white',
     fontWeight: '900'
+  },
+  listView:{
+    marginHorizontal: 20,
+  },
+  listText:{
+    fontSize: 16,
+    marginVertical: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    color :'white',
+    backgroundColor: 'red',
+  },
+  pressedStyle:{
+    opacity: 0.3
   }
 });
 
